97a2a5eb379ce7da92c4daa5ebc36760931a8454
SalemAlashour2 <90504661+SalemAlashour2@users.noreply.github.com>
2022-05-11 13:09:32 +0300

    First commit

---
+++ Multimedia_ITE_HW-1/Multimedia_ITE_HW-1/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Multimedia_ITE_HW_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.open_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayScale_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.red_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.green_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.blue_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.cyan_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.magenta_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.yellow_tsmi = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic.Location = new System.Drawing.Point(0, 24);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(1008, 663);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            this.pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_MouseDown);
+            this.pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_MouseMove);
+            this.pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.colorModeToolStripMenuItem,
+            this.colorsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open_tsmi,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // open_tsmi
+            // 
+            this.open_tsmi.Name = "open_tsmi";
+            this.open_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.open_tsmi.Text = "Open";
+            this.open_tsmi.Click += new System.EventHandler(this.open_tsmi_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // colorModeToolStripMenuItem
+            // 
+            this.colorModeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayScale_tsmi});
+            this.colorModeToolStripMenuItem.Name = "colorModeToolStripMenuItem";
+            this.colorModeToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.colorModeToolStripMenuItem.Text = "Color Mode";
+            // 
+            // grayScale_tsmi
+            // 
+            this.grayScale_tsmi.Name = "grayScale_tsmi";
+            this.grayScale_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.grayScale_tsmi.Text = "GrayScale";
+            this.grayScale_tsmi.Click += new System.EventHandler(this.grayScale_tsmi_Click);
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.red_tsmi,
+            this.green_tsmi,
+            this.blue_tsmi,
+            this.cyan_tsmi,
+            this.magenta_tsmi,
+            this.yellow_tsmi});
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.colorsToolStripMenuItem.Text = "Colors";
+            // 
+            // red_tsmi
+            // 
+            this.red_tsmi.Name = "red_tsmi";
+            this.red_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.red_tsmi.Text = "Red";
+            this.red_tsmi.Click += new System.EventHandler(this.red_tsmi_Click);
+            // 
+            // green_tsmi
+            // 
+            this.green_tsmi.Name = "green_tsmi";
+            this.green_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.green_tsmi.Text = "Green";
+            this.green_tsmi.Click += new System.EventHandler(this.green_tsmi_Click);
+            // 
+            // blue_tsmi
+            // 
+            this.blue_tsmi.Name = "blue_tsmi";
+            this.blue_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.blue_tsmi.Text = "Blue";
+            this.blue_tsmi.Click += new System.EventHandler(this.blue_tsmi_Click);
+            // 
+            // cyan_tsmi
+            // 
+            this.cyan_tsmi.Name = "cyan_tsmi";
+            this.cyan_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.cyan_tsmi.Text = "Cyan";
+            this.cyan_tsmi.Click += new System.EventHandler(this.cyan_tsmi_Click);
+            // 
+            // magenta_tsmi
+            // 
+            this.magenta_tsmi.Name = "magenta_tsmi";
+            this.magenta_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.magenta_tsmi.Text = "Magenta";
+            this.magenta_tsmi.Click += new System.EventHandler(this.magenta_tsmi_Click);
+            // 
+            // yellow_tsmi
+            // 
+            this.yellow_tsmi.Name = "yellow_tsmi";
+            this.yellow_tsmi.Size = new System.Drawing.Size(152, 22);
+            this.yellow_tsmi.Text = "Yellow";
+            this.yellow_tsmi.Click += new System.EventHandler(this.yellow_tsmi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 687);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1024, 768);
+            this.MinimumSize = new System.Drawing.Size(1024, 726);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem open_tsmi;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayScale_tsmi;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem red_tsmi;
+        private System.Windows.Forms.ToolStripMenuItem green_tsmi;
+        private System.Windows.Forms.ToolStripMenuItem blue_tsmi;
+        private System.Windows.Forms.ToolStripMenuItem cyan_tsmi;
+        private System.Windows.Forms.ToolStripMenuItem magenta_tsmi;
+        private System.Windows.Forms.ToolStripMenuItem yellow_tsmi;
+    }
+}
+
---
+++ Multimedia_ITE_HW-1/Multimedia_ITE_HW-1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multimedia_ITE_HW_1
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics graphics;
+
+        int x = -1;
+        int y = -1;
+
+        bool isMove = false;
+
+        Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+
+            graphics = pic.CreateGraphics();
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            pen = new Pen(Color.Black, 5);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        private void pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+
+            isMove = true;
+        }
+
+        private void pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMove && x != -1 && y != -1)
+            {
+                graphics.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+
+            this.Text = "X :  " + e.X + "  Y : " + e.Y;
+        }
+
+        private void pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMove = true;
+            x = -1;
+            y = -1;
+        }
+
+        private void open_tsmi_Click(object sender, EventArgs e)
+        {
+            OpenImageFromDrive();
+        }
+
+
+
+
+
+
+
+        private void OpenImageFromDrive()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "*.JPG;*.PNG;*.GIF;*.JPEG|*.JPG;*.PNG;*.GIF;*.JPEG";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pic.Image = Image.FromFile(ofd.FileName);
+                Bitmap bmp =new Bitmap(pic.Image) ;
+
+                BitmapData bmpData = bmp.LockBits(
+                    new Rectangle(0, 0, bmp.Width, bmp.Height),
+                    ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private void grayScale_tsmi_Click(object sender, EventArgs e)
+        {
+
+                Bitmap pic_img = new Bitmap(pic.Image);
+                Bitmap bmp = new Bitmap(pic.Image);
+
+            unsafe
+            {
+                BitmapData bmpData = bmp.LockBits(
+                    new Rectangle(0, 0, bmp.Width, bmp.Height),
+                    ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(bmp.PixelFormat) / 8;
+                int heightInPixel = bmpData.Height;
+                int widthInBytes = bmpData.Width * bytesPerPixel;
+
+                byte* ptrFirstPixel = (byte*)bmpData.Scan0;
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.WriteLine(ptrFirstPixel[i]);
+                }
+                return;
+                System.Threading.Tasks.Parallel.For(0, heightInPixel, y =>
+                {
+                    byte* currentLine = ptrFirstPixel + (y * bmpData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        int oldBlue = currentLine[x];
+                        int oldGreen = currentLine[x + 1];
+                        int oldRed = currentLine[x + 2];
+
+
+                        int avg = (oldBlue + oldGreen + oldRed) / 3;
+                        Color color = Color.FromArgb(1, avg, avg, avg);
+
+                        currentLine[x] = color.B;
+                        currentLine[x + 1] = color.G;
+                        currentLine[x + 2] = color.R;
+                    }
+                });
+
+                bmp.UnlockBits(bmpData);
+
+                pic.Image = bmp;
+            }
+
+        }
+
+        private void red_tsmi_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Red;
+        }
+
+        private void green_tsmi_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Green;
+        }
+
+        private void blue_tsmi_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Blue;
+        }
+
+        private void cyan_tsmi_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Cyan;
+        }
+
+        private void magenta_tsmi_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Magenta;
+        }
+
+        private void yellow_tsmi_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Yellow;
+        }
+    }
+}
